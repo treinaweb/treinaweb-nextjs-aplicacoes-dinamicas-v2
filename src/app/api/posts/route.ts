cbd8cb188b596ebaef202b3c0db4d4d020164b94
Wesley Gado <wesleygado@gmail.com>
2024-10-17 14:35:49 -0300

    Construindo API - Método POST

--- src/app/api/posts/route.ts
+++ src/app/api/posts/route.ts
@@ -1,5 +1,5 @@
+import { BlogPost } from "@/data/@types/BlogPostInterface";
 import axios from "axios";
-import next from "next";
 import { NextRequest, NextResponse } from "next/server";
 
 export const PostsApis = axios.create({
@@ -14,7 +14,22 @@ export async function GET(req: NextRequest) {
     const posts = await PostsApis.get("/");
     return NextResponse.json(posts.data, { status: 200 });
   } catch (error) {
-    return NextResponse.json({ error: "Falha ao carregar posts"}, { status: 500 });
+    return NextResponse.json({ error: "Falha ao carregar posts" }, { status: 500 });
   }
 }
 
+export async function POST(req: NextRequest) {
+  try {
+    const body = await req.json();
+    const newPost = body as BlogPost;
+    newPost.slug = newPost.title
+      .toLowerCase()
+      .replace(/\s/g, '-')
+      .replace(/[^\w-]+/g, '');
+
+    const createdPost = await PostsApis.post('/', newPost);
+    return NextResponse.json(createdPost.data, { status: 201 });
+  } catch (error) {
+    return NextResponse.json({ error: "Erro ao cadastrar post" }, { status: 500 })
+  }
+}
